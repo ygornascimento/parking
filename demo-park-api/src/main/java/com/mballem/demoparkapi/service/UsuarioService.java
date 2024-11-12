11880435c355d8708fbbeadfd73ce1342bbe01e9
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-11-12 14:56:45 -0300

    Adicionado o restante da arquitetura.

---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.mballem.demoparkapi.service;
+
+import com.mballem.demoparkapi.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+}
---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.mballem.demoparkapi.web.controller;
+
+import com.mballem.demoparkapi.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/usuarios")
+public class UsuarioController {
+    private final UsuarioService usuarioService;
+}
