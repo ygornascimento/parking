814ac45082b1dd627633c738ab603a4f0ddca18a
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-11-14 06:51:29 -0300

    Adicionado feature para alterar senha do usuario.

--- demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/service/UsuarioService.java
@@ -1,11 +1,32 @@
 package com.mballem.demoparkapi.service;
 
+import com.mballem.demoparkapi.entity.Usuario;
 import com.mballem.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Usuário não encontrado")
+        );
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
 }
