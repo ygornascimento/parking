92b142fd0105cd49645c39569da531e45ccd5c2a
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-11-25 19:02:56 -0300

    Adicionado informações ao Swagger na recuperação de senha.

--- demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
@@ -70,6 +70,24 @@ public class UsuarioController {
         return ResponseEntity.ok(UsuarioMapper.toDTO(user));
     }
 
+
+    @Operation(summary = "Atualizar senha.",
+            description = "Atualizar senha.",
+            responses = {
+                    @ApiResponse(responseCode = "204",
+                            description = "Senha atualizada com sucesso.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = UsuarioResponseDTO.class))),
+                    @ApiResponse(responseCode = "400",
+                            description = "Senha não confere.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "404",
+                            description = "Recurso não encontrado.",
+                            content = @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            }
+    )
     @PatchMapping("/{id}")
     public ResponseEntity<Void> updatePassword(@PathVariable Long id, @Valid @RequestBody UsuarioSenhaDTO dto) {
         Usuario user = usuarioService.editarSenha(id, dto.getSenhaAtual(), dto.getNovaSenha(), dto.getConfirmarSenha());
