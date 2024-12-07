f7c950444a9c595791bdf0a05a6460747bf79406
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-12-07 08:33:02 -0300

    Configurações iniciais do Spring Security e JWT

---
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/jwt/JwtUserDetails.java
@@ -0,0 +1,26 @@
+package com.mballem.demoparkapi.jwt;
+
+import com.mballem.demoparkapi.entity.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+
+    private Usuario usuario;
+
+    public JwtUserDetails(Usuario usuario) {
+        super(usuario.getUsername(), usuario.getPassword(), AuthorityUtils.createAuthorityList(usuario.getRole().name()));
+        this.usuario = usuario;
+    }
+
+    public Long getId() {
+        return this.usuario.getId();
+    }
+
+    public String getRole() {
+        return this.usuario.getRole().name();
+    }
+}
