4bcc30a167ec1125f4ade05f861d9ccb6a6978f5
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-12-10 16:04:08 -0300

    Teste ponto a ponto para Create (Status 403, 409 e 422)

--- demo-park-api/src/test/java/com/mballem/demoparkapi/ClienteIT.java
+++ demo-park-api/src/test/java/com/mballem/demoparkapi/ClienteIT.java
@@ -1,6 +1,7 @@
 package com.mballem.demoparkapi;
 import com.mballem.demoparkapi.web.dto.ClienteCreateDTO;
 import com.mballem.demoparkapi.web.dto.ClienteResponseDTO;
+import com.mballem.demoparkapi.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,5 +35,84 @@ public class ClienteIT {
         assertThat(responseBody.getId()).isNotNull();
         assertThat(responseBody.getNome()).isEqualTo("Tobias Ferreira");
         assertThat(responseBody.getCpf()).isEqualTo("68222388088");
+    }
+
+    @Test
+    public void criarCliente_ComCpfCadastrado_RetornarCliente_ComStatus_409() {
+        ErrorMessage responseBody = webTestClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Tobias Ferreira", "39998186030"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(409);
+    }
+
+    @Test
+    public void criarCliente_ComDadosInvalidos_RetornarCliente_ComStatus_422() {
+        ErrorMessage responseBody = webTestClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Bobb", "00000000000"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
+
+        responseBody = webTestClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "toby@email.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Bobb", "399.981.860-30"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void criarCliente_ComUsuarioNaoPermitido_RetornarCliente_ComStatus_403() {
+        ErrorMessage responseBody = webTestClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(webTestClient, "ana@email.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Tobias Ferreira", "39998186030"))
+                .exchange()
+                .expectStatus().isEqualTo(403)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        assertThat(responseBody).isNotNull();
+        assertThat(responseBody.getStatus()).isEqualTo(403);
     }
 }
