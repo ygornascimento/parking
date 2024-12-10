37e83f4503febc81a35636776fb5bfe174ae9076
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-12-10 16:14:43 -0300

    Recurso para localizar um cliente pelo identificador

--- demo-park-api/src/main/java/com/mballem/demoparkapi/service/ClienteService.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/service/ClienteService.java
@@ -2,8 +2,9 @@ package com.mballem.demoparkapi.service;
 
 import com.mballem.demoparkapi.entity.Cliente;
 import com.mballem.demoparkapi.exception.CpfUniqueViolationException;
+import com.mballem.demoparkapi.exception.EntityNotFoundException;
 import com.mballem.demoparkapi.repository.ClienteRepository;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,12 @@ public class ClienteService {
             throw new CpfUniqueViolationException(String
                     .format("CPF '%s' não pode ser cadastrado, já existe no sistema.", cliente.getCpf()));
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Cliente buscarPorId(Long id) {
+        return clienteRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Cliente id=%s não encontrado no sistema.", id))
+        );
     }
 }
--- demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/ClienteController.java
+++ demo-park-api/src/main/java/com/mballem/demoparkapi/web/controller/ClienteController.java
@@ -18,10 +18,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -61,5 +58,11 @@ public class ClienteController {
         clienteService.save(cliente);
 
         return ResponseEntity.status(201).body(ClienteMapper.toDto(cliente));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteResponseDTO> getById(@PathVariable Long id) {
+        Cliente cliente = clienteService.buscarPorId(id);
+        return ResponseEntity.ok(ClienteMapper.toDto(cliente));
     }
 }
